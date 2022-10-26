ede642cb0d9d8acb390ace82f18782429f8ec691
Mark Gil B. Utbo <111882468+marked12@users.noreply.github.com>
2022-10-26 12:38:24 +0800

    Update Developers.tsx
    div-change

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -7,7 +7,7 @@ const Developers = () => {
         <div className='p-5 m-5 grid grid-cols-6 gap-2'>            
                 {/* Add your profile here. You can upload your image to the repo under "images"  directory
                     and link it here.                
-                */
+                */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
                         <img src="images/profile.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
@@ -15,7 +15,7 @@ const Developers = () => {
                     <h3 className='font-bold mr-1'>Mark Gil Utbo, </h3>
                     <p className='font-thin italic'>Front-End Developer</p>
                 </div>
-          }
+          
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
